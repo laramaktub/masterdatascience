2642dacc1ac7393b91abd92ee4f3adde705a36c3
root <root@wiki.ifca.es>
2020-09-09 14:04:37 +0200

    first commit

---
+++ more/demo-contacts.php
@@ -0,0 +1,17 @@
+<?php
+if( isset($_POST['name']) )
+{
+	$to = 'gsrthemes9@gmail.com'; // Replace with your email
+	
+	$subject = $_POST['subject'];
+	$message = $_POST['message'] . "\n\n" . 'Regards, ' . $_POST['name'] . '.';
+	$headers = 'From: ' . $_POST['name'] . "\r\n" . 'Reply-To: ' . $_POST['email'] . "\r\n" . 'X-Mailer: PHP/' . phpversion();
+	
+	mail($to, $subject, $message, $headers);
+	
+	if( $_POST['copy'] == 'on' )
+	{
+		mail($_POST['email'], $subject, $message, $headers);
+	}
+}
+?>
